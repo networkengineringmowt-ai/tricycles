4bd115d522e5c4f3ffcb0cd0b06c402eb85a259a
Network Engineering - MOWT <networkengineringmowt@gmail.com>
2026-08-05 10:07:12 +0300

    Update Map to use OSM basemap and bounds

--- website/src/components/InfographicDashboard.jsx
+++ website/src/components/InfographicDashboard.jsx
@@ -49,10 +49,10 @@ const InfographicDashboard = () => {
           </div>
         </div>
         <div style={{ height: '400px', borderRadius: '8px', overflow: 'hidden', margin: '15px 0', border: '1px solid var(--border-color)' }}>
-          <MapContainer center={[0.3163, 32.5811]} zoom={13} style={{ height: '100%', width: '100%' }}>
+          <MapContainer bounds={[[0.2981, 32.5469], [0.3458, 32.5761]]} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
             <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-              attribution='&copy; OpenStreetMap &copy; CARTO'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             {studySites.map((site, idx) => (
               <Marker 
